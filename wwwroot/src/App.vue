d8e594613fe204708d5c864c8edbbbf0759552c4
liyuanwu <16619891743@163.com>
2023-05-20 07:11:03 +0800

    first commit

---
+++ wwwroot/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <TopNav></TopNav>
+</template>
+
+<script>
+import TopNav from './components/home/TopNav.vue';
+export default {
+  name: 'App',
+  components: {
+    TopNav
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.icon {
+  width: .5rem;
+  height: .5rem;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
---
+++ wwwroot/src/views/MusicView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="top-nav">
+    <div class="top-left">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-musicfavoriteslist"></use>
+      </svg>
+    </div>
+    <div class="top-center">
+      <span>我的</span>
+      <span>发现</span>
+      <span>云村</span>
+      <span>视频</span>
+    </div>
+    <div class="top-left">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-musicsearchlist"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<style>
+.top-nav{
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
+
+<script>
+
+</script>
